049fef891ad6d552f4812fadd4cbd2aa836efc8b
alanhett <ahettinger@gmail.com>
2024-08-28 09:56:07 -0400

    Rebuild

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sid = sid;
 const crypto_1 = __importDefault(require("crypto"));
-// Defaults to base 32 alphabet, 5 bits per character, 25 characters, 125 bits
+// Default base 58 alphabet, 5.85798 bits per character, 22 characters, 128.9 random bits
 // Compare to UUIDv4, which has 122 random bits
-// Omits 0, o, i, l, u to improve readability in cases where id may need to be manually read
-function sid({ length = 25, alphabet = '123456789abcdefghjkmnpqrstuvwxyz', prefix = '', } = {}) {
+// Omits 0, O, I, l for readability
+function sid({ length = 22, alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz', prefix = '', } = {}) {
     const arr = new Array(length);
     for (let i = 0; i < length; i++) {
         arr[i] = alphabet[crypto_1.default.randomInt(alphabet.length)];
