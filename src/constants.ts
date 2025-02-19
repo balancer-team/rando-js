bfc1894064782487cf775476981dd217e34d9eb2
alanhett <ahettinger@gmail.com>
2025-02-19 16:46:02 -0500

    Added base 50

--- src/constants.ts
+++ src/constants.ts
@@ -4,6 +4,7 @@
 
 export const BASE_64 = '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz'
 export const BASE_62 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+export const BASE_50 = '123456789BCDFGHJKLMNPQRSTVWXYZbcdfghjkmnpqrstvwxyz'
 export const BASE_44 = '123456789BCDFGHJKLNPRSTVXYZbcdfghknpqrstvxyz'
 export const BASE_27 = '123456789BCDFGHJKLNPRSTVXYZ'
 
